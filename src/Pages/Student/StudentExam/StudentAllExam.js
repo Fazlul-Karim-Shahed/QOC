c0c614676d9e066d7d3b21e01054c7e1e1d4d859
Fazlul-Karim-Shahed <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>
2024-03-28 22:57:44 +0600

    ui fix exam

--- src/Pages/Student/StudentExam/StudentAllExam.js
+++ src/Pages/Student/StudentExam/StudentAllExam.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getExamByIdApi } from '../../../Api/Student/ExamApi'
 import Spinner from '../../../components/Spinner'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faArrowRight, faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
 
 const mapStateToProps = (state) => {
     return {
@@ -40,14 +42,21 @@ export const StudentAllExam = (props) => {
         examShow = exam.map(item => {
 
             return (
-                <Link aria-disabled to={'/student-dashboard/exam'} state={{ exam: item }} className='card card-body glass my-5 hover:shadow-lg bg-sky-700 text-white'>
-                    <div className='card-title mb-5'>
-                        {item.subjectId.subject} - {item.exam} 
-                    </div>
-                    <div className='card-text'>Start - {new Date(item.startTime).toLocaleString('en-US', { hour12: true, timeZone: 'Asia/Dhaka' })}</div>
-                    <div className='card-text'>End - {new Date(item.endTime).toLocaleString('en-US', { hour12: true, timeZone: 'Asia/Dhaka' })}</div>
+                <div className='card  glass my-5 hover:shadow-lg bg-sky-700 text-white'>
+                    <div className='text-center mb bg-zinc-700 rounded-t-lg p-2 shadow-lg'>Participants - {item.participants ? item.participants.length : 0}</div>
+                    <Link aria-disabled to={'/student-dashboard/exam'} state={{ exam: item }} className='card-body'>
 
-                </Link>
+
+
+                        <div className='card-title mb-5'>
+                            {item.subjectId.subject} <FontAwesomeIcon className='mt-1' icon={faArrowRightLong} /> {item.exam}
+                        </div>
+                        <div className='card-text'>Start - {new Date(item.startTime).toLocaleString('en-US', { hour12: true, timeZone: 'Asia/Dhaka' })}</div>
+                        <div className='card-text'>End - {new Date(item.endTime).toLocaleString('en-US', { hour12: true, timeZone: 'Asia/Dhaka' })}</div>
+
+
+                    </Link>
+                </div>
             )
         })
     }
@@ -55,7 +64,8 @@ export const StudentAllExam = (props) => {
 
     return (
         <div>
-            <div className='p-2 text-center mb-10 capitalize font-bold text-2xl'>{message}</div>
+            <div className=' text-center mb-10 capitalize font-bold text-2xl'>{message}</div>
+            <div className='text-xs italic'>**Exam will automatically remove after 24 hour from exam time. You can find from your respective course section.</div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>{examShow}</div>
 
